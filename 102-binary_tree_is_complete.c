21a634b09c341347bba8390e1356998e33f9e554
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-27 23:45:35 +0200

    Check if a binary tree is complete.

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,51 +1,54 @@
 #include "binary_trees.h"
 
 /**
+ * get_size - measures the size of a binary tree.
+ * @tree: pointer to the root node of the tree.
+ *
+ * Return: the tree's size or 0 if tree is NULL.
+ */
+int get_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (get_size(tree->left) + get_size(tree->right) + 1);
+}
+
+/**
+ * check_complete - Check if a binary tree is complete.
+ * @root: Pointer to the root node of the tree to check
+ * @index: starting node index
+ * @number_nodes: total number of the tree nodes
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int check_complete(const binary_tree_t *root, int index, int number_nodes)
+{
+	if (root == NULL)
+		return (1);
+
+	if (index >= number_nodes)
+		return (0);
+
+	return (check_complete(root->left, 2 * index + 1, number_nodes) &&
+			check_complete(root->right, 2 * index + 2, number_nodes));
+}
+
+/**
  * binary_tree_is_complete - Checks if a binary tree is complete
  * @tree: Pointer to the root node of the tree to check
  * Return: 1 if the tree is complete, 0 otherwise
  *
- * Description: A tree is considered a complete binary tree, if it adheres to
- * these two rules:
- * 1. All levels except the last are completely filled
- * 2. The last level, if not completely filled, must have all nodes as far left
- *    as possible.
- *
+ * Theroms:
+ * A complete binary tree is a tree in which all the levels are completely
+ * filled except possibly the lowest one, which is filled from the left.
+ * It is just like a full binary tree, but with two major differences:
+ * 1. All the leaf elements must lean towards the left side.
+ * 2. The last leaf element might not have a right sibling.
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	const binary_tree_t **queue, *current;
-	int lvl_has_ended = 0, front = 0, rear = -1;
-
 	if (tree == NULL)
 		return (0);
 
-	queue = malloc(sizeof(binary_tree_t *));
-	if (queue == NULL)
-		return (0);
-
-	queue[++rear] = tree;
-	while (front <= rear)
-	{
-		current = queue[front++];
-		/* If we have encountered a NULL node, mark the end of the level */
-		if (current == NULL)
-			lvl_has_ended = 1;
-		else
-		{
-			/* If we have previously encountered the end of the level, */
-			/* but the current node is not NULL, then the tree is not complete */
-			if (lvl_has_ended)
-				return (0);
-			/* Enqueue the left and right children */
-			queue = realloc(queue, (rear + 2) * sizeof(binary_tree_t *));
-			if (queue == NULL)
-				return (0);
-			queue[++rear] = current->left;
-			queue[++rear] = current->right;
-		}
-	}
-	/* If we reach here, the tree is complete */
-	free(queue);
-	return (1);
+	return (check_complete(tree, 0, get_size(tree)));
 }
