682896a433de983ab95c5d187af6719e7b61e5cf
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-27 22:48:52 +0200

    add check for when left node is present without the right node or the other way around.

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,7 +13,9 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-
+	if ((tree->left && tree->right == NULL) ||
+		(tree->left == NULL && tree->right))
+		return (0);
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 
