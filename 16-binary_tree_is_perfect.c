1639c0cc72ce732393daf271737c6fe792851c29
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-03-27 23:12:16 +0200

    Check if a binary tree is perfect.

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - measures the depth of a node in a binary tree.
- * @tree: pointer to the root node of the tree.
- *
+ * get_depth - measures the depth of a node in a binary tree.
+ * @node: pointer to the root node of the tree.
  * Return: the tree's depth or 0 if tree is NULL.
  */
 int get_depth(const binary_tree_t *node)
